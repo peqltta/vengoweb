bd872ee2a8e5a957e6213e50817ad70d1537ea3a
peqltta <85615860+peqltta@users.noreply.github.com>
2021-09-29 22:25:36 +0100

    asdfasdf
    asdfasdf

--- vengo.py
+++ vengo.py
@@ -5,6 +5,9 @@ app = Flask(__name__,static_url_path="", static_folder="static")
 @app.route("/")
 def hello_world():
     return render_template('index.html')
-
+@app.route("/update", methods=['GET','POST'])
+def update():
+    subprocess.run(["git","pull"])
+    return "updated"
 app.run(host='0.0.0.0', port='5000', debug=True)
 
