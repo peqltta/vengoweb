1d5843f27118d42ff99d70cc34d43083458935af
elle1594 <90333078+elle1594@users.noreply.github.com>
2021-09-30 11:44:41 +0100

    vfs

--- vengo.py
+++ vengo.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-
+import subprocess
 app = Flask(__name__,static_url_path="", static_folder="static")
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
-
-app.run()
+@app.route("/update", methods=['GET','POST'])
+def update():
+    subprocess.run(["sh","update.sh"])
+    return "updated"
+app.run(host='0.0.0.0', port='5000', debug=True)
 
